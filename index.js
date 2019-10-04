10192ead1fd3928782d2de780c959698351187cd
AlexanderGrxnt <alexandergrxnt@gmail.com>
2019-10-04 21:19:38 +0100

    voice test

--- index.js
+++ index.js
@@ -67,7 +67,44 @@ function triggerCharacters() {
   });
    //triggerCharacters();
 
+   //initialization of speech recognition starts here
+const artyom = new Artyom();
+var commands = [
+ {
+	 indexes:["up to no good"],
+	 action:function() {
+    triggerCharacters();
+	 }
+ }, {
+	 indexes:["mischief managed"],
+	 action:function() {
+		 $('.map-base').removeClass('active');
+	 }
+ }
+]
+
+artyom.addCommands(commands);
+
+function startContinuousArtyom(){
+    artyom.fatality();
+
+    setTimeout(function(){
+         artyom.initialize({
+            lang:"en-GB",
+            continuous:true,
+            listen:true, 
+            speed:1
+        }).then(function(){
+            console.log("Ready to work !");
+        });
+    },250);
+}
+
+startContinuousArtyom();
+
 });
+
+
 
 // function closeWolf(){
 // const wolf = document.getElementById("wolf");
