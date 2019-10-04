7d865787f6bf87b34a7686d752e346560518c077
AlexanderGrxnt <alexandergrxnt@gmail.com>
2019-10-04 21:49:34 +0100

    changed voice command

--- index.js
+++ index.js
@@ -1,106 +1,117 @@
 jQuery(document).ready(function () {
-    // Go through a sentence, wrap its characters with spans
-    function setUpCharacters() {
-        var $sentences = $('.sentence');
+  // Go through a sentence, wrap its characters with spans
+  function setUpCharacters() {
+    var $sentences = $('.sentence');
 
-        // Run for each sentence
-        $sentences.each(function () {
-            var $sentence = $(this);
-            var newContent = '';
+    // Run for each sentence
+    $sentences.each(function () {
+      var $sentence = $(this);
+      var newContent = '';
 
-            // Go through all characters of the sentence
-            for (i = 0; i < $sentence.text().length; i++) {
-                var substring = $sentence.text().substr(i, 1);
+      // Go through all characters of the sentence
+      for (i = 0; i < $sentence.text().length; i++) {
+        var substring = $sentence.text().substr(i, 1);
 
-                // If we have a character, wrap it
-                if (substring != " ") {
-                    newContent += '<span>' + substring + '</span>';
-                } else {
-                    newContent += substring;
-                }
-            }
+        // If we have a character, wrap it
+        if (substring != " ") {
+          newContent += '<span>' + substring + '</span>';
+        } else {
+          newContent += substring;
+        }
+      }
 
-            // Replace content
-            $sentence.html(newContent);
-        });
-    }
+      // Replace content
+      $sentence.html(newContent);
+    });
+  }
 
-    setUpCharacters();
+  setUpCharacters();
 
-    // Go through a sentence and trigger activate state
-function triggerCharacters() {
+  // Go through a sentence and trigger activate state
+  function triggerCharacters() {
     var sentenceCounter = 0;
     var sentenceDelay = 600;
-  
-    $('.sentence').each(function() {
+
+    $('.sentence').each(function () {
       var $sentence = $(this);
-  
+
       // Trigger for each sentence
-      setTimeout(function() {
+      setTimeout(function () {
         var $spans = $sentence.find('span');
         var spanCounter = 0;
         var spanDelay = 75;
-  
+
         // Loop through all spans and activate
-        $spans.each(function() {
+        $spans.each(function () {
           var $span = $(this);
-  
+
           // Trigger a timeout so each span is offset
-          setTimeout(function() {
+          setTimeout(function () {
             $span.toggleClass('active');
           }, (spanCounter * spanDelay));
-  
-          spanCounter++; 
+
+          spanCounter++;
         });
       }, (sentenceCounter * sentenceDelay));
-  
+
       sentenceCounter++;
     });
   }
-  
+
+  function FadeWolf() {
+    $("#wolf").fadeOut(2000, function () {
+      // Animation complete
+    });
+  }
+
   // For our example, trigger character animations on button click
-  $('.button').on('click', function() {
+  $('.button').on('click', function () {
     triggerCharacters();
-    $( "#wolf" ).fadeOut( 2000, function() {
+    $("#wolf").fadeOut(2000, function () {
       // Animation complete
     });
   });
-   //triggerCharacters();
+  //triggerCharacters();
 
-   //initialization of speech recognition starts here
-const artyom = new Artyom();
-var commands = [
- {
-	 indexes:["up to no good"],
-	 action:function() {
-    triggerCharacters();
-	 }
- }, {
-	 indexes:["mischief managed"],
-	 action:function() {
-		 $('.map-base').removeClass('active');
-	 }
- }
-]
+  //initialization of speech recognition starts here
+  const artyom = new Artyom();
+  var commands = [
+    {
+      indexes: ["reveal yourself"],
+      action: function () {
+        triggerCharacters();
+      }
+    }, {
+      indexes: ["mischief managed"],
+      action: function () {
+        triggerCharacters();
+      }
+    }, {
+      indexes: ["reveal yourself"],
+      action: function () {
+        FadeWolf();
+      }
+    }
+  ]
 
-artyom.addCommands(commands);
+  artyom.addCommands(commands);
 
-function startContinuousArtyom(){
+  function startContinuousArtyom() {
     artyom.fatality();
 
-    setTimeout(function(){
-         artyom.initialize({
-            lang:"en-GB",
-            continuous:true,
-            listen:true, 
-            speed:1
-        }).then(function(){
-            console.log("Ready to work !");
-        });
-    },250);
-}
+    setTimeout(function () {
+      artyom.initialize({
+        lang: "en-GB",
+        continuous: true,
+        listen: true,
+        speed: 1
+      }).then(function () {
+        console.log("Ready to work !");
+      });
+    }, 250);
+  }
 
-startContinuousArtyom();
+  startContinuousArtyom();
 
 });
 
@@ -118,17 +129,17 @@ startContinuousArtyom();
 
 // });
 
-function openHTMLNav(){
-  if(window.screen.width < 768)
-      document.getElementById("overlayNav-HTML").style.width ="200px";
+function openHTMLNav() {
+  if (window.screen.width < 768)
+    document.getElementById("overlayNav-HTML").style.width = "200px";
   else
-      document.getElementById("overlayNav-HTML").style.width ="180px";
-  document.getElementById("blankPanel").style.width ="100%";
-  
+    document.getElementById("overlayNav-HTML").style.width = "180px";
+  document.getElementById("blankPanel").style.width = "100%";
+
 }
 
-function closeNav(){
-  document.getElementById("overlayNav-HTML").style.width ="0%";
-    
-  document.getElementById("blankPanel").style.width ="0%";
+function closeNav() {
+  document.getElementById("overlayNav-HTML").style.width = "0%";
+
+  document.getElementById("blankPanel").style.width = "0%";
 }
